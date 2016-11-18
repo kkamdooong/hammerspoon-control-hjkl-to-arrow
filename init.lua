8c12cf46d20bb049c65210a300123120d0bd0a15
kkamdooong <kkamdooong@gmail.com>
2016-11-18 16:10:34 +0900

    make faster repeat key stroke
    check this issue https://github.com/Hammerspoon/hammerspoon/issues/1011#issuecomment-261114434

--- init.lua
+++ init.lua
@@ -1,7 +1,13 @@
 local function keyCode(key, modifiers)
   modifiers = modifiers or {}
-  return function() hs.eventtap.keyStroke(modifiers, key) end
+
+  return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+  end
 end
+
 
 hs.hotkey.bind({'ctrl'}, 'h', keyCode('left'), nil, keyCode('left'))
 hs.hotkey.bind({'ctrl'}, 'j', keyCode('down'), nil, keyCode('down'))
